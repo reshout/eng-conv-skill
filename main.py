498c44120ad8714d937558beb46f8423160a9e5a
reshout <reshout@gmail.com>
2017-05-02 20:35:14 +0900

    playing conversation feature complete

--- main.py
+++ main.py
@@ -5,105 +5,63 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
-        },
-        'reprompt': {
+def build_text_response(output_speech, should_end_session, session_attributes=None):
+    response = {
+        'version': '1.0',
+        'response': {
             'outputSpeech': {
                 'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
+                'text': output_speech
+            },
+            'shouldEndSession': should_end_session
+        }
     }
+    if session_attributes:
+        response['sessionAttributes'] = session_attributes
+    return response
 
 
-def build_response(session_attributes, speechlet_response):
-    return {
+def build_audio_response(audio_url):
+    response = {
         'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
+        'response': {
+            'outputSpeech': {
+                'type': 'SSML',
+                'ssml': '<speak><audio src="{}" /></speak>'.format(audio_url)
+            },
+            'shouldEndSession': True,
+        }
     }
+    return response
 
 
 def get_welcome_response():
-    session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+    output_speech = 'You can say simply, "play 7". The number can be between 1 and 100.'
+    return build_text_response(output_speech, False)
 
 
 def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-
-
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
+    output_speech = 'Have a nice day!'
+    return build_text_response(output_speech, True)
 
 
 def on_launch(request, session):
     logger.debug('request={}, session={}'.format(request, session))
+    return get_welcome_response()
+
+
+def play_particular(intent, session):
+    conv_no = intent['slots']['conv_no']
+    if 'value' not in conv_no:
+        return get_welcome_response()
+    conv_no = int(conv_no['value'])
+    if conv_no >= 0 and conv_no <= 100:
+        return build_audio_response('https://s3.amazonaws.com/eng-conv-skill/DAY{:03d}.mp3'.format(conv_no))
+    else:
+        return get_welcome_response()
+
+
+def play_random(intent, session):
     return get_welcome_response()
 
 
@@ -112,16 +70,16 @@ def on_intent(request, session):
     intent = request['intent']
     intent_name = request['intent']['name']
 
-    if intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
-    elif intent_name == "AMAZON.HelpIntent":
+    if intent_name == 'AMAZON.HelpIntent':
         return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+    elif intent_name == 'AMAZON.CancelIntent' or intent_name == 'AMAZON.StopIntent':
         return handle_session_end_request()
+    elif intent_name == 'PlayParticular':
+        return play_particular(intent, session)
+    elif intent_name == 'PlayRandom':
+        return play_random(intent, session)
     else:
-        raise ValueError("Invalid intent")
+        raise ValueError('Invalid intent')
 
 
 def lambda_handler(event, context):
@@ -129,11 +87,11 @@ def lambda_handler(event, context):
     request_type = request['type']
     session = event['session']
 
-    if request_type == "LaunchRequest":
+    if request_type == 'LaunchRequest':
         formatter = logging.Formatter('{} %(filename)s:%(lineno)d %(funcName)s() %(message)s\n'.format(request_type), '%H:%M:%S')
         logger.handlers[0].setFormatter(formatter)
         return on_launch(request, session)
-    elif request_type == "IntentRequest":
+    elif request_type == 'IntentRequest':
         intent_name = request['intent']['name']
         formatter = logging.Formatter('{} %(filename)s:%(lineno)d %(funcName)s() %(message)s\n'.format(intent_name), '%H:%M:%S')
         logger.handlers[0].setFormatter(formatter)
